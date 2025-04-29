fcb2b661b9cd551bfcdeec8570f17c983e6453d9
tranduchoangnam <20febnam@gmail.com>
2025-04-30 01:31:18 +0700

    feat: deploy config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Chuyển các lỗi thành cảnh báo cho các quy tắc cụ thể
+      '@typescript-eslint/no-unused-vars': 'warn',         // Chuyển lỗi không sử dụng biến thành cảnh báo
+      'react/no-unescaped-entities': 'warn',              // Chuyển lỗi không thoát kí tự thành cảnh báo
+      '@typescript-eslint/no-explicit-any': 'warn',       // Chuyển lỗi sử dụng `any` thành cảnh báo
+    },
+  },
 ];
 
 export default eslintConfig;
